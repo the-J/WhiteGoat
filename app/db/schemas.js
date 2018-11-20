343f20b8d4ed682e1aac26280b7e23f9c67da0e6
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-20 19:56:17 +0100

    extending TwitchChannels schema

--- app/db/schemas.js
+++ app/db/schemas.js
@@ -4,18 +4,19 @@
 
 const Sequelize = require('sequelize');
 const sequelize = require('./init.js');
-
-const User = sequelize.define('user', {
-    username: Sequelize.STRING,
-    birthday: Sequelize.DATE
-});
+//
+// const User = sequelize.define('user', {
+//     username: Sequelize.STRING,
+//     birthday: Sequelize.DATE
+// });
 
 const TwitchChannels = sequelize.define('TwitchChannels', {
     chanelName: Sequelize.STRING,
-    message: { type: Sequelize.STRING, defaultValue: 'is live now' }
+    message: { type: Sequelize.STRING, defaultValue: 'is live now' },
+    tags: {type: Sequelize.ARRAY(Sequelize.TEXT), defaultValue: []}
 });
 
 sequelize.sync();
 
-module.exports = User;
+// module.exports = User;
 module.exports = TwitchChannels;
