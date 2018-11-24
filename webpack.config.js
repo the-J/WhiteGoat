48f005cf1f4907b6c94c616d9efde64bdc967688
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-24 10:47:19 +0100

    updating license

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+    mode: 'production',
     entry: './index.js',
     target: 'node',
-
     externals: [
         nodeExternals(),
         { pg: true }
