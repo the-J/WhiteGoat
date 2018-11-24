f9f2e1d4084041b533c2e8911e5068d3e510b610
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-24 14:17:44 +0100

    schemas

--- app/db/schemas.js
+++ app/db/schemas.js
@@ -5,13 +5,23 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./init.js');
 
-const TwitchChannels = sequelize.define("TwitchChannels", {
+const BOT = require('../credentials/botCredentials.js');
+const botOwner = BOT.OWNER;
+
+const TwitchChannels = sequelize.define('TwitchChannels', {
     chanelName: Sequelize.STRING, // twitch.tv chanel name
     chanelId: Sequelize.STRING, // twitch.tv chanel id
     profileImageUrl: Sequelize.STRING, // twitch.tv profile image
     message: { type: Sequelize.STRING, defaultValue: ' is live now' }, // message to display when channel goes life
-    userIds: {type: Sequelize.ARRAY(Sequelize.TEXT), defaultValue: []}, // assigned by user, mentioned in a message
-    streaming: {type: Sequelize.BOOLEAN, defaultValue: false} // update if streaming
+    userIds: { type: Sequelize.ARRAY(Sequelize.TEXT), defaultValue: [] }, // assigned by user, mentioned in a message
+    streaming: { type: Sequelize.BOOLEAN, defaultValue: false } // update if streaming
+});
+
+const TwitchSettings = sequelize.define('TwitchSettings', {
+    chanelId: Sequelize.STRING, // text channel id on which bot will answer
+    admins: { type: Sequelize.ARRAY(Sequelize.TEXT), defaultValue: [] }, // bot administrators
+    ownerId: { type: Sequelize.TEXT, defaultValue: botOwner } // bot administrators
 });
 
 module.exports.TwitchChannels = TwitchChannels;
+module.exports.TwitchSettings = TwitchSettings;
