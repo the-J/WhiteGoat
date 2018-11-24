a6728b165a0ba998700fb2dacb3ce447eb55320e
Juliusz Jakubowski <the-j@users.noreply.github.com>
2018-11-24 21:25:45 +0100

    cosmetics

--- app/bot/methods/messagesAdmin.js
+++ app/bot/methods/messagesAdmin.js
@@ -2,10 +2,7 @@
  * Created by juliusz.jakubowski@gmail.com on 24.11.18.
  */
 const messages = require('./messages.js');
-const twitchListener = require('./twitchListener.js');
-const twitch = require('../../twitch/apiMethods.js');
 const dbTwitch = require('../../db/methodsTwitch.js');
-const bot = require('../bot.js');
 const BOT = require('../../credentials/botCredentials.js');
 
 const prefix = BOT.PREFIX;
@@ -22,25 +19,28 @@ const handleAdminMessages = async function ( message ) {
 
     switch (key) {
         case 'setchanelid':
-            // set chanel on which bot should be responding
-            if (!params[ 1 ] || !params[ 1 ].length) {
-
-                response.content = 'I need more info, turu tutu';
-                return messages.sendMessage(response);
-            }
-
-            const chanelId = params[ 1 ].substring(2, params[ 1 ].length - 1);
-
-            await dbTwitch.updateTwitchSettings({ chanelId })
-                .then(settings => {
-                    if (settings) {
-                        response.content = 'All set up! I\'ll respond on <#' + chanelId + '>';
-                        return messages.sendMessage(response);
-                    }
-                })
-                .catch(err => console.log('update chanelid err:', err));
-            return;
+            return setChanelId(params, response);
     }
 };
+
+async function setChanelId( params, response ) {
+    // set chanel on which bot should be responding
+    if (!params[ 1 ] || !params[ 1 ].length) {
+
+        response.content = 'I need more info, turu tutu';
+        return messages.sendMessage(response);
+    }
+
+    const chanelId = params[ 1 ].substring(2, params[ 1 ].length - 1);
+
+    await dbTwitch.updateTwitchSettings({ chanelId })
+        .then(settings => {
+            if (settings) {
+                response.content = 'All set up! I\'ll respond on <#' + chanelId + '>. Remember to call ```!update```';
+                return messages.sendMessage(response);
+            }
+        })
+        .catch(err => console.log('update chanelid err:', err));
+}
 
 module.exports.handleAdminMessages = handleAdminMessages;
